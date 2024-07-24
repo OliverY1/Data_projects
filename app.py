59c1a500c40d5360b98f3f6b994c622351a5379c
OliverY1 <166818916+OliverY1@users.noreply.github.com>
2024-07-24 21:36:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ hide_streamlit_style = """
             <style>
             [data-testid="stToolbar"] {visibility: hidden !important;}
             footer {visibility: hidden !important;}
+            header {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
